46881256fe20f314ff0da493cfb653106688ac34
blagojevic-nj <66080426+blagojevic-nj@users.noreply.github.com>
2023-07-26 10:31:58 +0200

    feat: starting crud over accounts implemented (#4)

--- src/main/java/buky/example/userservice/controller/UserController.java
+++ src/main/java/buky/example/userservice/controller/UserController.java
@@ -4,10 +4,7 @@ import buky.example.userservice.exceptions.NotFoundException;
 import buky.example.userservice.model.User;
 import buky.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,5 +25,17 @@ public class UserController {
     public User getUserById(@PathVariable Long id) {
         Optional<User> user = userService.getUserById(id);
         return user.orElseThrow(() -> new NotFoundException("User with this id is not found!"));
+    }
+
+    @PutMapping("/{username}")
+    public User updateUserById(@PathVariable String username, @RequestBody User updatedUser) {
+        //TODO iz tokena uzimati
+        return userService.updateUser(username, updatedUser);
+    }
+
+    @DeleteMapping("/{username}")
+    public void updateUserById(@PathVariable String username) {
+        //TODO iz tokena uzimati kad se odradi security
+        userService.deleteUser(username);
     }
 }
