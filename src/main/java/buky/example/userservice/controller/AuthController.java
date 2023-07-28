e9a285c57ce6a7fbb899b76bea6c5c50e3ac1890
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-07-28 10:51:45 +0200

    feat: additional security between services implemented

--- src/main/java/buky/example/userservice/controller/AuthController.java
+++ src/main/java/buky/example/userservice/controller/AuthController.java
@@ -3,13 +3,15 @@ package buky.example.userservice.controller;
 import buky.example.userservice.dto.LoginDto;
 import buky.example.userservice.dto.Token;
 import buky.example.userservice.model.User;
+import buky.example.userservice.security.TokenUtils;
 import buky.example.userservice.service.AuthService;
 import buky.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -18,6 +20,7 @@ public class AuthController {
 
     private final UserService userService;
     private final AuthService authService;
+    private final TokenUtils tokenUtils;
 
     @PostMapping("/login")
     public Token login(@RequestBody LoginDto loginDto) {
@@ -27,5 +30,15 @@ public class AuthController {
     @PostMapping("/registration")
     public User registration(@RequestBody User user) {
         return userService.registration(user);
+    }
+
+    @GetMapping("/authenticate")
+    public ResponseEntity<Void> authenticateUser(@RequestHeader("Authorization") String jwtToken,
+                                                 @RequestHeader("X-User-Role") List<String> userRole) {
+        if (tokenUtils.isValidUser(jwtToken, userRole)) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 }
