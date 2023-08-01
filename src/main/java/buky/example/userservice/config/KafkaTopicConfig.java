5716033fd36d6c7498b1017a4fffd7a2744b0f83
blagojevic-nj <66080426+blagojevic-nj@users.noreply.github.com>
2023-08-01 17:09:20 +0200

    feat: rating notifications handling implemented (#11)

--- src/main/java/buky/example/userservice/config/KafkaTopicConfig.java
+++ src/main/java/buky/example/userservice/config/KafkaTopicConfig.java
@@ -2,11 +2,18 @@ package buky.example.userservice.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,10 +24,25 @@ public class KafkaTopicConfig {
     private String bootstrapAddress;
 
     @Bean
+    public ProducerFactory<String, Serializable> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "20971520");
+        return new DefaultKafkaProducerFactory<>(configProps, new StringSerializer(), new JsonSerializer<>());
+    }
+
+    @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
         configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    KafkaTemplate<String, Serializable> jsonKafkaTemplate(ProducerFactory<String, Serializable> jsonProducerFactory) {
+        return new KafkaTemplate<>(jsonProducerFactory);
     }
 
     @Bean
@@ -31,5 +53,20 @@ public class KafkaTopicConfig {
     @Bean
     public NewTopic productTopic2() {
         return new NewTopic("topic2", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic hostRatingTopic() {
+        return new NewTopic("host-rating", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic accommodationRatingTopic() {
+        return new NewTopic("accommodation-rating", 1, (short) 1);
+    }
+
+    @Bean
+    public NewTopic notificationsTopic() {
+        return new NewTopic("notifications-topic", 1, (short) 1);
     }
 }
