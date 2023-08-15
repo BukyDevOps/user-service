00fe11b5e556b8c3857c9a27eb12651eb452295c
blagojevic-nj <blagojevic.njegos1999@gmail.com>
2023-08-15 11:41:36 +0200

    feat: cors implemented

--- src/main/java/buky/example/userservice/config/WebSecurityConfig.java
+++ src/main/java/buky/example/userservice/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception
     {
-
+        http.cors(AbstractHttpConfigurer::disable);
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(eh -> eh.authenticationEntryPoint(restAuthenticationEntryPoint))
