d9f1ce827637ade8e54b95db0877451c144ec6b7
Luka Kureljušić <63510902+Lule99@users.noreply.github.com>
2023-08-15 20:07:12 +0200

    feat: clr (#20)

--- src/main/java/buky/example/userservice/UserServiceApplication.java
+++ src/main/java/buky/example/userservice/UserServiceApplication.java
@@ -1,13 +1,43 @@
 package buky.example.userservice;
 
+import buky.example.userservice.model.User;
+import buky.example.userservice.model.enums.Role;
+import buky.example.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
-public class UserServiceApplication {
+@RequiredArgsConstructor
+public class UserServiceApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var host = User.builder()
+				.username("host")
+				.password(passwordEncoder.encode("123"))
+				.role(Role.HOST)
+				.active(true)
+				.build();
+
+		var guest = User.builder()
+				.username("guest")
+				.password(passwordEncoder.encode("123"))
+				.role(Role.GUEST)
+				.active(true)
+				.build();
+
+		userRepository.saveAll(List.of(host, guest));
+	}
 }
