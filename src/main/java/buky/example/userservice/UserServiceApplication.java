f4a2f38324173bf9d63290cd83ff04bfd86e6663
Luka Kureljušić <63510902+Lule99@users.noreply.github.com>
2023-08-25 12:33:39 +0200

    feat: elk stack test run (#22)

--- src/main/java/buky/example/userservice/UserServiceApplication.java
+++ src/main/java/buky/example/userservice/UserServiceApplication.java
@@ -1,25 +1,25 @@
 package buky.example.userservice;
 
-import buky.example.userservice.model.User;
-import buky.example.userservice.model.enums.Role;
 import buky.example.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceApplication {
 
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+		log.info("Welcome home Page " + LocalDateTime.now());
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
